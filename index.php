c15fe215eee15eeadaf09908fe0297b2855a1130
Louis Raphael <lraphael@sanfrancisco.travel>
2014-06-27 10:40:07 -0700

    new changes
    new changes

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ include 'head.php';
 ?>
 <body>
 <form action="insert.php" method="post">
-First name: <input type="text" name="firstname">
-Last name: <input type="text" name="lastname">
-Age: <input type="number" name="age">
+First name: <input type="text" required name="firstname">
+Last name: <input type="text" required name="lastname">
+Age: <input type="number" required name="age">
 <input type="submit"  name="submit">
 </form>
 <a href="http://localhost:8888/list-all.php">List all records</a>
 
 
-test
+
 </body>
 </html>
